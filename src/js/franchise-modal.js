79149452ce4f0951d7ded59ae80590db5386ad0b
Valera Kamelkov <tahaburrent@gmail.com>
2022-09-17 20:02:02 +0300

    1

--- src/js/franchise-modal.js
+++ src/js/franchise-modal.js
@@ -9,7 +9,7 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('franchise-modal-open');
+    document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('modal-is-hidden');
   }
 })();

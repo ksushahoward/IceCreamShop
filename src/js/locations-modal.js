287288cfdd7e1622e016d116d732bcb5186a0356
Valera Kamelkov <tahaburrent@gmail.com>
2022-09-17 22:56:43 +0300

    ~

---
+++ src/js/locations-modal.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-locations-modal-open]'),
+    closeModalBtn: document.querySelector('[data-locations-modal-close]'),
+    modal: document.querySelector('[data-locations-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('modal-is-hidden');
+  }
+})();

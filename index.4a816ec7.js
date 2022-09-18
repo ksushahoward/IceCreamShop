6c7a3ad941aabc47d479636282cd9e152d75746a
ksushahoward <108617945+ksushahoward@users.noreply.github.com>
2022-09-18 06:10:50 +0000

    Deploying to gh-pages from @ ksushahoward/goit-team-project-group-6@553f80823b60ac625e30cdccc5178abbbd434476 🚀

--- index.4a816ec7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-franchise-modal-open]"),closeModalBtn:document.querySelector("[data-franchise-modal-close]"),modal:document.querySelector("[data-franchise-modal]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("modal-is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.4a816ec7.js.map

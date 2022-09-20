2e2e90d20a6f891a7239954ba4a6ecca1497cd0a
Jeka <107277013+Karavang@users.noreply.github.com>
2022-09-20 21:07:00 +0200

    width

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/franchise-modal';
 import './js/mobile-menu';
 import './js/locations-modal';
-import './js/read-more-modal';
+/*import './js/read-more-modal';*/
 import './js/slider';
 
